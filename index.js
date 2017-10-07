43b808affcd475e7ad94e4f494c306334323dbb3
Khalil Ahmed <mkhalila@outlook.com>
2017-10-07 22:06:36 +0100

    Use templates to cleanup query

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 const getJSON = require('get-json');
 
 const overpass = 'http://overpass-api.de/api/interpreter?data=';
-const query = '[out:json];way[%22highway%22](around:50,51.424037,-0.148666);out;';
+
+const lat = 51.424037;
+const long = -0.148666;
+const distance = 50;
+
+const query = `[out:json]; way["highway"](around:${distance},${lat},${long}); out;`;
 
 getJSON(`${overpass}${query}`, (err, result) => {
   if (err) console.log(err);
