73802ba80c8b9aa31d206798380fc043417e9355
Khalil Ahmed <mkhalila@outlook.com>
2017-10-07 23:55:52 +0100

    Parse query to only extract road names

--- index.js
+++ index.js
@@ -1,14 +1,20 @@
 const getJSON = require('get-json');
+const unique = require('array-unique');
 
 const overpass = 'http://overpass-api.de/api/interpreter?data=';
 
 const lat = 51.424037;
 const long = -0.148666;
-const distance = 50;
+const distance = 100;
 
 const query = `[out:json]; way["highway"](around:${distance},${lat},${long}); out;`;
 
 getJSON(`${overpass}${query}`, (err, result) => {
   if (err) console.log(err);
-  console.log(result);
+
+  let roads = [];
+  result.elements.forEach(element => roads.push(element.tags.name));
+  roads = unique(roads);
+
+  console.log(roads);
 });
